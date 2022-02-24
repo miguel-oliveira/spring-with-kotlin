c67f9c141ed0342c2a5d3ff613d6b5f0a56a0d05
Miguel Oliveira <miguel.nicolau.oliveira762@gmail.com>
2022-02-24 22:07:26 +0000

    Add base project

---
+++ src/main/kotlin/miguel/oliveira/demo/kotlin/MySpringDemoKotlinApplication.kt
@@ -0,0 +1,11 @@
+package miguel.oliveira.demo.kotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MySpringDemoKotlinApplication
+
+fun main(args: Array<String>) {
+    runApplication<MySpringDemoKotlinApplication>(*args)
+}
---
+++ src/test/kotlin/miguel/oliveira/demo/kotlin/MySpringDemoKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package miguel.oliveira.demo.kotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MySpringDemoKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
